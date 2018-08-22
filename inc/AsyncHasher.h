37ced1eddb81cc580af4e34546a6b088a3fb4329
Neil Williams <neewill@gmail.com>
2018-08-22 23:37:15 +0100

    Generalise the async hashing function
    Can now be used for strong, weak or any hashing function

--- inc/AsyncHasher.h
+++ inc/AsyncHasher.h
@@ -2,9 +2,18 @@
 
 #include <experimental/filesystem>
 
+#include <future>
+#include <queue>
+
+#include "Files.h"
+#include "Hashes.h"
+
 namespace diffsync {
 
 namespace fs = std::experimental::filesystem;
+
+// TODO: this function could be generalised further as it doesn't really
+// have anything to do with hashes.
 
 /**
  * Reads the file in blocks from the current thread and calculates
@@ -12,7 +21,33 @@ namespace fs = std::experimental::filesystem;
  *
  * Returns a collection of the hashes from the file.
  */
-std::vector<std::vector<unsigned char>>
-asyncHash(const fs::path& filePath, uint32_t blockSize);
+template <typename HashT, typename HashF>
+std::vector<HashT>
+asyncHash(const fs::path& filePath, uint32_t blockSize, HashF hashFunction) {
+    std::queue<std::future<HashT>> hashFutures;
+
+    diffsync::readFile(filePath, blockSize,
+            [&hashFutures, &hashFunction] (const std::vector<unsigned char>& blockData) {
+
+        auto future = std::async(std::launch::async, hashFunction, blockData);
+        hashFutures.push(std::move(future));
+    });
+
+    std::vector<HashT> hashes;
+    hashes.reserve(hashFutures.size());
+
+    while (hashFutures.size() > 0) {
+        hashes.push_back(hashFutures.front().get());
+        hashFutures.pop();
+    }
+
+    return hashes;
+}
+
+/**
+ * Reads the file in blocks and collects both the strong and the weak hashes.
+ */
+std::vector<std::tuple<uint32_t, std::vector<unsigned char>>>
+asyncStrongAndWeakHash(const fs::path& filePath, uint32_t blockSize);
 
 }
--- test/AsyncHasher_test.cpp
+++ test/AsyncHasher_test.cpp
@@ -5,11 +5,7 @@
 
 namespace fs = std::experimental::filesystem;
 
-TEST(AsyncHasherTest, returnCount) {
-    fs::path path("testdata/1k_binary_blob");
-    auto hashes = diffsync::asyncHash(path, 100);
-
-    std::vector<std::string> expectedHashes = {
+static const std::vector<std::string> EXPECTED_STRONG_HASHES = {
         "5e7a73eb221c7a35a2852eccd0d27cd4",
         "e1d09567de47dcf761e715dd1fcdc5c8",
         "cd31626e4389b124c15ae69d276bae40",
@@ -22,10 +18,46 @@ TEST(AsyncHasherTest, returnCount) {
         "b8cab0615154664f31382970a3f4472c",
         "454e7f98c00dfc87324690663a92570e"};
 
+static const std::vector<uint32_t> EXPECTED_WEAK_HASHES = {
+    12341, 12378, 13395, 13280, 13207, 12650, 12020, 12746, 12139, 11769, 3229
+};
+
+TEST(AsyncHasherTest, strongHashes) {
+    fs::path path("testdata/1k_binary_blob");
+    auto hashes =
+        diffsync::asyncHash<std::vector<unsigned char>>(
+            path, 100, &diffsync::strongHash);
+
     std::vector<std::string> actualHashes;
     std::transform(hashes.begin(), hashes.end(),
         std::back_inserter(actualHashes),
         &diffsync::hashToHexString);
 
-    ASSERT_EQ(actualHashes, expectedHashes);
+    ASSERT_EQ(actualHashes, EXPECTED_STRONG_HASHES);
+}
+
+TEST(AsyncHasherTest, weakHashes) {
+    fs::path path("testdata/1k_binary_blob");
+    auto hashes =
+        diffsync::asyncHash<uint32_t>(
+            path, 100, &diffsync::weakHash);
+
+    ASSERT_EQ(hashes, EXPECTED_WEAK_HASHES);
+}
+
+TEST(AsyncHasherTest, strongAndWeakHashes) {
+    fs::path path("testdata/1k_binary_blob");
+
+    auto hashes = diffsync::asyncStrongAndWeakHash(path, 100);
+
+    std::vector<uint32_t> weakHashes;
+    std::vector<std::string> strongHashes;
+
+    for(auto&&[weak, strong] : hashes) {
+        weakHashes.push_back(weak);
+        strongHashes.push_back(diffsync::hashToHexString(strong));
+    }
+
+    ASSERT_EQ(strongHashes, EXPECTED_STRONG_HASHES);
+    ASSERT_EQ(weakHashes, EXPECTED_WEAK_HASHES);
 }
